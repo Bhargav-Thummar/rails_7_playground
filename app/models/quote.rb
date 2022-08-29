21452a193a4b1ac899ede2c13268273422879da3
Bhargav-Thummar <bhargav.thummar810@gmail.com>
2022-08-29 12:14:07 +0530

    feat: Add a quote total with Turbo Frames

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,6 +3,7 @@ class Quote < ApplicationRecord
   belongs_to :company
   has_many :comments, dependent: :destroy
   has_many :line_item_dates, dependent: :destroy
+  has_many :line_items, through: :line_item_dates
 
   # validations
   validates :name, presence: true
@@ -15,4 +16,8 @@ class Quote < ApplicationRecord
   after_create_commit -> { broadcast_prepend_to [company, "quotes"] }
   after_update_commit -> { broadcast_replace_to [company, "quotes"], target: "main__quote_#{self.id}" }
   after_destroy_commit -> { broadcast_remove_to [company, "quotes"], target: "main__quote_#{self.id}" }
+
+  def total_price
+    line_items.sum(&:total_price)
+  end
 end
